f5650915f3d3fcd9f5b9b488d6fb4a238495006b
niamh-d <niamh.linguist@gmail.com>
2023-09-21 16:55:56 +0300

    added cities data, and components CityList and CityItem

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,13 +2,14 @@ import AppNav from "./AppNav";
 import Logo from "./Logo";
 import Footer from "./Footer";
 import styles from "./Sidebar.module.css";
+import { Outlet } from "react-router-dom";
 
 function SideBar() {
   return (
     <div className={styles.sidebar}>
       <Logo />
       <AppNav />
-      <p>List of cities</p>
+      <Outlet />
       <Footer />
     </div>
   );
